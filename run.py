e2e5b88c5658f77a9d7e4883cd865b292096ed2b
Jdizzydizzle <139468339+JWalshe86@users.noreply.github.com>
2023-10-12 18:04:08 +0100

    feat: add user informed what pizza & cost
    this info is presented to user when an option is selected

--- run.py
+++ run.py
@@ -25,10 +25,9 @@ def get_order_request():
     print("Please answer 'yes' or 'no'")
     print("Example: yes")
 
-    user_input_data = input("Enter 'yes' or 'no' here:\n")
-    print(f"You said {user_input_data}")
-    validate_user_input_data(user_input_data)
-
+    user_input_yesno_data = input("Enter 'yes' or 'no' here:\n")
+    print(f"You said {user_input_yesno_data}")
+    validate_user_yes_no(user_input_yesno_data)
 
 def display_menu(data):
     """
@@ -36,7 +35,7 @@ def display_menu(data):
     """
     print(tabulate(data, headers=["Option", "Name", "Price(€)"], numalign="center", tablefmt="double_outline"),)
 
-def validate_user_input_data(input_data):
+def validate_user_yes_no(input_data):
     """
     validate if user has inputted yes or no string and if not they are informed they need to do so
     """
@@ -45,6 +44,8 @@ def validate_user_input_data(input_data):
         if input_data == "yes":
             print("Great! here's the menu...\n\n")
             display_menu(menu_data)
+            pizza_option = input("Enter a number between 1 and 5 here:\n")
+            get_user_menu_options(pizza_option)
         elif input_data == "no":
             print("Thats okay, hope to see you again soon")
         else:
@@ -53,6 +54,14 @@ def validate_user_input_data(input_data):
             )
     except ValueError as e:
         print(f"Invalid entry: {e}, please try again\n")
-
+        
+def get_user_menu_options(pizza_option):
+    """return the options the user chose and add to worksheet named 'order'
+    """
+    i = pizza_option
+    pizza_names = menu.cell(i,2).value
+    
+    pizza_price = menu.cell(i,3).value
+    print(f'You have chosen {pizza_names} at a cost of €{pizza_price}')
 
 get_order_request()
