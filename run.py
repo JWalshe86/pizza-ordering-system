fc3266e309f01332698650eba584f53d2572a2a2
Jdizzydizzle <139468339+JWalshe86@users.noreply.github.com>
2023-10-12 20:13:11 +0100

    feat: add error msg response for pizza option

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from tabulate import tabulate
+from termcolor import colored
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -19,49 +20,71 @@ menu = SHEET.worksheet("menu")
 # all the order sheet data
 menu_data = menu.get_all_values()
 
+
 def get_order_request():
     """Get order request input from the user"""
+    print(
+        "\033[1m" + "Welcome to " + colored("Nags with Notions!", "red") + "\033[0m\n"
+    )
     print("Do you want to order?")
-    print("Please answer 'yes' or 'no'")
-    print("Example: yes")
+    print("Please answer 'yes' or 'no'\n")
 
     user_input_yesno_data = input("Enter 'yes' or 'no' here:\n")
-    print(f"You said {user_input_yesno_data}")
+    print(f"You said {user_input_yesno_data}\n\n")
     validate_user_yes_no(user_input_yesno_data)
+
 
 def display_menu(data):
     """
     displays the menu as per the google sheet menu page
     """
-    print(tabulate(data, headers=["Option", "Name", "Price(€)"], numalign="center", tablefmt="double_outline"),)
+    print(
+        tabulate(
+            data,
+            headers=["Option", "Name", "Price(€)"],
+            numalign="center",
+            tablefmt="double_outline",
+        ),
+    )
 
 def validate_user_yes_no(input_data):
     """
-    validate if user has inputted yes or no string and if not they are informed they need to do so
+    validate if user inputs yes or no string and if not they are informed they need to do so
     """
 
     try:
         if input_data == "yes":
-            print("Great! here's the menu...\n\n")
+            print("Here is our" + "\033[1m" + " pizza menu")
             display_menu(menu_data)
             pizza_option = input("Enter a number between 1 and 5 here:\n")
-            get_user_menu_options(pizza_option)
+            validate_get_user_menu_options(pizza_option)
         elif input_data == "no":
             print("Thats okay, hope to see you again soon")
         else:
             raise ValueError(
-                f"Exactly yes or no answer required, you said {(input_data)}"
+                f"Number must be between 1 and 5, you said {(input_data)}"
             )
     except ValueError as e:
         print(f"Invalid entry: {e}, please try again\n")
-        
-def get_user_menu_options(pizza_option):
-    """return the options the user chose and add to worksheet named 'order'
-    """
-    i = pizza_option
-    pizza_names = menu.cell(i,2).value
+
+def validate_get_user_menu_options(pizza_option):
+    """_summary_
+validate if the user input is an integer between 1-5 
+if not the user is informed that they need to re-enter their option
     
-    pizza_price = menu.cell(i,3).value
-    print(f'You have chosen {pizza_names} at a cost of €{pizza_price}')
+    """
+    try:
+        if int(pizza_option)>=1 and int(pizza_option)<=5:
+            i = pizza_option
+            pizza_names = menu.cell(i, 2).value
+            pizza_price = menu.cell(i, 3).value
+            print(f"You have chosen {pizza_names} at a cost of €{pizza_price}")
+        else:
+            raise ValueError(
+                f"Exactly yes or no answer required, you said {(pizza_option)}"
+            )
+    except ValueError as e:
+        print(f"Invalid entry: {e}, please try again\n")
+
 
 get_order_request()
