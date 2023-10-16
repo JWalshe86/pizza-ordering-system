5732e81f8fd37917bb1381dc7e2bc4bc86f45327
Jdizzydizzle <139468339+JWalshe86@users.noreply.github.com>
2023-10-16 14:58:59 +0100

    feat: add indentity argument to validator function
    input_identity (string) _description_ returns a value so the
    validator knows if it's validating
    user input for pizza selection or
    user input for quantity selection

--- run.py
+++ run.py
@@ -23,33 +23,47 @@ menu_data = menu.get_all_values()
 RUN_INPUT = True
 
 
-def user_input_validator(data):
+def user_input_validator(data, input_identity):
     """Check if user has inputted an integer between 1 - 5
 
     Args:
-        data (integer): _description_
+        data (integer): _description_ the numerical option the user takes whether it's 1-5 
+        for pizza or 1-10 for quantity.
+        input_identity (string) _description_ returns a value so the validator knows if it's validating
+        user input for pizza selection or user input for quantity selection
 
     Returns:
         _type_: boolean_description_if no errors returns True
     """
-    try:
-        if int(data) >= 1 and int(data) <= 5:
-            i = data
-            pizza_names = menu.cell(i, 2).value
-            pizza_price = menu.cell(i, 3).value
-            print(f"You have chosen {pizza_names} at a cost of €{pizza_price}")
-            user_order_quantity_request()
-            order = SHEET.worksheet("order")
-            # adds order to order worksheet
-            order.append_row([f"{pizza_names}", f"{pizza_price}"])
-        else:
-            raise ValueError(print(f"Answer must be 1 - 5, you said {data}"))
-    except ValueError as e:
-        print(f"\nInvalid entry: {e}, please try again\n")
-        request_pizza_option_number()
-        return False
-        # if an error occurs
+    while RUN_INPUT is True:
+        
+        try:
+            if input_identity == 'pizza_option' and int(data) >= 1 and int(data) <= 5:
+                i = data
+                pizza_names = menu.cell(i, 2).value
+                pizza_price = menu.cell(i, 3).value
+                print(f"You have chosen {pizza_names} at a cost of €{pizza_price}")
+                user_order_quantity_request()
+                order = SHEET.worksheet("order")
+                # adds order to order worksheet
+                order.append_row([f"{pizza_names}", f"{pizza_price}"])
+            
+            elif input_identity == 'order_quantity' and int(data) >= 1 and int(data) <= 10:
+                print(f'you chose: {data}')
+                exit()
 
+            else:
+                raise ValueError(print(f"Answer must be 1 - 5, you said {data}"))
+        except ValueError as e:
+            if input_identity == 'pizza_option':
+                print(f"\nInvalid pizza option entry: {e}, please try again\n")
+                request_pizza_option_number()
+        
+            elif input_identity == 'order_quantity':
+                print(f"\nInvalid quantity entry: {e}, please try again\n")
+            
+        # if an error occurs
+                return False
     # if the function runs without any errors
     return RUN_INPUT is True
     pass
@@ -81,7 +95,7 @@ def request_pizza_option_number():
     """request number option from user"""
 
     pizza_option_number = input("Enter a number between 1 and 5 here:\n")
-    user_input_validator(pizza_option_number)
+    user_input_validator(pizza_option_number, 'pizza_option')
 
 def user_order_quantity_request():
     """function to get the amount of product the user has ordered
@@ -97,6 +111,7 @@ def user_order_quantity_request():
         order_quantity = input("\033[1m" + "Write your answer here and"
                             " press Enter when you're ready:\n")
         print('order quantity:', order_quantity)
+        user_input_validator(order_quantity, 'order_quantity')
         
 def main():
     """functions which I wish to run everytime"""
